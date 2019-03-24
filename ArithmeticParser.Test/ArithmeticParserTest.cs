a872ae889eb1c76dc7d0a711d09ac85c49f1ea3f
Thomas Bruderer <apophis@apophis.ch>
2019-03-24 17:05:10 +0100

    Use the injected Parser class for the tests

--- ArithmeticParser.Test/ArithmeticParserTest.cs
+++ ArithmeticParser.Test/ArithmeticParserTest.cs
@@ -2,18 +2,27 @@
 using System.Linq;
 using ArithmeticParser.Nodes;
 using ArithmeticParser.Visitors;
+using Autofac;
 using Xunit;
 
 namespace ArithmeticParser.Test
 {
     public class ArithmeticParserTest
     {
+        private readonly Parser _parser;
+
+        public ArithmeticParserTest()
+        {
+            _parser = new CompositionRoot()
+                .Build()
+                .Resolve<Parser>();
+        }
+
 
         [Fact]
         public void ParseSimpleArithmeticExpressionCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("5+17*22");
+            var parseTree = _parser.Parse("5+17*22");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -24,8 +33,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ParseSingleNumberCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("22");
+            var parseTree = _parser.Parse("22");
 
             var number = parseTree as NumberNode;
             Assert.NotNull(number);
@@ -45,8 +53,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ParseSimpleFloatCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("3.14159265");
+            var parseTree = _parser.Parse("3.14159265");
 
             var number = parseTree as NumberNode;
             Assert.NotNull(number);
@@ -66,8 +73,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ParseSimpleAdditionCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("1+2+3+4+5+6+7+8+9+10+11+12+13+14+15+16+17+18+19+20");
+            var parseTree = _parser.Parse("1+2+3+4+5+6+7+8+9+10+11+12+13+14+15+16+17+18+19+20");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -78,8 +84,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ParseSimpleSubtractionCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("100-1-2-3-4-5-6-7-8-9)");
+            var parseTree = _parser.Parse("100-1-2-3-4-5-6-7-8-9)");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -91,9 +96,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ParseSimpleMultiplicationCorrectly()
         {
-            Parser parser = new Parser();
-
-            var parseTree = parser.Parse("1*2*3*4*5*6*7");
+            var parseTree = _parser.Parse("1*2*3*4*5*6*7");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -104,9 +107,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ParseSimpleDivisionCorrectly()
         {
-            Parser parser = new Parser();
-
-            var parseTree = parser.Parse("(1/2)/(1/10)/2");
+            var parseTree = _parser.Parse("(1/2)/(1/10)/2");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -117,9 +118,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ParseComplexArithmeticExampleCorrectly()
         {
-            Parser parser = new Parser();
-
-            var parseTree = parser.Parse("145.2/3+7-(8*45+22*(2-19))-88/8 + 17");
+            var parseTree = _parser.Parse("145.2/3+7-(8*45+22*(2-19))-88/8 + 17");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -135,16 +134,14 @@ namespace ArithmeticParser.Test
         [Fact]
         public void CreatesArithmeticExpressionWithMinimalAmountOfParenthesis()
         {
-            Parser plusParser = new Parser();
-            var plusParseTree = plusParser.Parse("(1+(2+(3+4)))");
+            var plusParseTree = _parser.Parse("(1+(2+(3+4)))");
 
             var parenthesisVisitor = new MinimalParenthesisVisitor();
             plusParseTree.Accept(parenthesisVisitor);
 
             Assert.Equal("1+2+3+4", parenthesisVisitor.Result);
 
-            Parser minusParser = new Parser();
-            var minusParseTree = minusParser.Parse("(-1-(2-(3-4)))");
+            var minusParseTree = _parser.Parse("(-1-(2-(3-4)))");
 
             var minimalParenthesisVisitor = new MinimalParenthesisVisitor();
             minusParseTree.Accept(minimalParenthesisVisitor);
@@ -155,8 +152,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ShouldCreateMultiplicationWithMinimalAmountOfParenthesis()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("6*(1+2+3)");
+            var parseTree = _parser.Parse("6*(1+2+3)");
 
             var parenthesisVisitor = new MinimalParenthesisVisitor();
             parseTree.Accept(parenthesisVisitor);
@@ -167,8 +163,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ShouldCreatesDivisionWithMinimalAmountOfParenthesis()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("((24/1)/(2/3))");
+            var parseTree = _parser.Parse("((24/1)/(2/3))");
 
             var parenthesisVisitor = new MinimalParenthesisVisitor();
             parseTree.Accept(parenthesisVisitor);
@@ -179,8 +174,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ShouldSimplifyToExpressionWithoutParenthesis()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("(((7*8)+(5*6))+(10/5))");
+            var parseTree = _parser.Parse("(((7*8)+(5*6))+(10/5))");
 
             var parenthesisVisitor = new MinimalParenthesisVisitor();
             parseTree.Accept(parenthesisVisitor);
@@ -191,8 +185,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void ShouldCreateExpressionWithMinimalParenthesisGivenExpressionWithVariablesAndFunctions()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("(((7*a)+(5*cos(Pi)))+(10/sqrt(c+(d*5))))");
+            var parseTree = _parser.Parse("(((7*a)+(5*cos(Pi)))+(10/sqrt(c+(d*5))))");
 
             var parenthesisVisitor = new MinimalParenthesisVisitor();
             parseTree.Accept(parenthesisVisitor);
@@ -203,8 +196,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void CalculateExpressionWithConstantsCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("2 * Pi * Pi");
+            var parseTree = _parser.Parse("2 * Pi * Pi");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -216,8 +208,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void CalculatesAFunctionCallCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("2 * cos(Pi)");
+            var parseTree = _parser.Parse("2 * cos(Pi)");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -229,8 +220,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void CalculatesFunctionCallsWithMultipleParametersCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("pow(sqrt(2), 2)");
+            var parseTree = _parser.Parse("pow(sqrt(2), 2)");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -242,8 +232,7 @@ namespace ArithmeticParser.Test
         [Fact]
         public void CalculatesComplexFunctionCallsWithMultipleParametersCorrectly()
         {
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("pow(pow(2,2), sqrt(2) * sqrt(2)) * e");
+            var parseTree = _parser.Parse("pow(pow(2,2), sqrt(2) * sqrt(2)) * e");
 
             var calculator = new CalculateVisitor();
             parseTree.Accept(calculator);
@@ -256,8 +245,7 @@ namespace ArithmeticParser.Test
         public void ShouldParseVariableXAndCalculateWithDifferentValuesOfXCorrectly()
         {
 
-            Parser parser = new Parser();
-            var parseTree = parser.Parse("100 * x");
+            var parseTree = _parser.Parse("100 * x");
 
             var calculator = new CalculateVisitor();
 
