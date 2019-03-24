664103aecee75d878ece15469df652377c49af1e
Thomas Bruderer <apophis@apophis.ch>
2019-03-24 16:58:02 +0100

    Add Autofac as DI Framework

---
+++ ArithmeticParser/ParserModule.cs
@@ -0,0 +1,19 @@
+﻿using ArithmeticParser.Parsing;
+using Autofac;
+
+namespace ArithmeticParser
+{
+    public class ParserModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Parser>().AsSelf();
+            builder.RegisterType<ExpressionParser>().AsSelf().PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies);
+            builder.RegisterType<TermParser>().AsSelf();
+            builder.RegisterType<FactorParser>().AsSelf();
+            builder.RegisterType<FunctionParser>().AsSelf();
+            builder.RegisterType<VariableParser>().AsSelf();
+            builder.RegisterType<FactorParser>().AsSelf();
+        }
+    }
+}
